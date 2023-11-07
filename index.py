44022abcf55d2862e46841a7ead16b2697d87704
Ling930616 <lingling930616@gmail.com>
2023-11-07 12:03:44 +0800

    make it better

--- index.py
+++ index.py
@@ -52,7 +52,7 @@ def read():
     collection_ref = db.collection("靜宜資管")    
     docs = collection_ref.get()    
     for doc in docs:         
-        Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
+       Result += "文件內容：{}".format(doc.to_dict()) + "<br>"    
     return Result
 
 #if __name__ == "__main__":
