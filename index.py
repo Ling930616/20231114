0e7abb0fc1a0a423075606e4a0dbf30babc07b3b
Ling930616 <lingling930616@gmail.com>
2023-11-13 14:22:13 +0800

    make it better

--- index.py
+++ index.py
@@ -1,61 +1,43 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
 
 from flask import Flask, render_template, request
 from datetime import datetime,timezone,timedelta
 app = Flask(__name__)
 
+
+
 @app.route("/")
 def index():
-    homepage = "<h1>朱芷伶Python網頁</h1>"
-    homepage += "<a href=/mis>MIS</a><br>"
-    homepage += "<a href=/today>顯示日期時間</a><br>"
-    homepage += "<a href=/welcome?nick=tcyang>傳送使用者暱稱</a><br>"
-    homepage += "<a href=/about>芷伶簡介網頁</a><br>"
-    homepage += "<a href=/account>網頁表單輸入帳密傳值</a><br>"
-    homepage += "<br><a href=/read>人選之人演員查詢名單</a><br>"
+    homepage = "<h1>朱芷伶個人網頁</h1>"
+    homepage += "<a href=/home>首頁</a><br>"
+    homepage += "<a href=/TearsofThemis>何倫碼</a><br>"
+    homepage += "<a href=/HonKaiStarRail>自傳履歷</a><br>"
+    homepage += "<a href=/HonkaiImpact3>公司</a><br>"
+
     return homepage
 
-@app.route("/mis")
-def course():
-    return "<h1>資訊管理導論</h1>"
 
-@app.route("/today")
-def today():
+@app.route("/home")
+def home():
     now = datetime.now()
-    return render_template("today.html", datetime = str(now))
+    return render_template("home.html")
 
-@app.route("/about")
-def about():
-    return render_template("about.html") 
+@app.route("/TearsofThemis")
+def TearsofThemis():
+    now = datetime.now()
+    return render_template("TearsofThemis.html") 
 
-@app.route("/welcome", methods=["GET", "POST"])
-def welcome():
-    user = request.values.get("nick")
-    return render_template("welcome.html", name=user)
+@app.route("/HonKaiStarRail")
+def HonKaiStarRail():
+    now = datetime.now()
+    return render_template("HonKaiStarRail.html") 
 
-@app.route("/account", methods=["GET", "POST"])
-def account():
-    if request.method == "POST":
-        user = request.form["user"]
-        pwd = request.form["pwd"]
-        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
-        return result
-    else:
-        return render_template("account.html")
-        
-@app.route("/read")
-def read():
-    Result = ""
-    db = firestore.client()
-    collection_ref = db.collection("人選之人-造浪者") 
-    docs = collection_ref.order_by("birth", direction=firestore.Query.DESCENDING).get()    
-    for doc in docs:
-        x = doc.to_dict()         
-        Result += "演員:{},劇中角色是{},出生於{}".format(x["name"],x["role"],x["birth"]) + "<br>"    
-    return Result
+@app.route("/HonkaiImpact3")
+def HonkaiImpact3():
+    now = datetime.now()
+    return render_template("HonkaiImpact3.html") 
+
+
 
 #if __name__ == "__main__":
-#   app.run()
+#  app.run()
+
