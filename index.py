ad461d78927b339161ac7a567244c58f636226cc
Ling930616 <lingling930616@gmail.com>
2023-12-05 11:42:05 +0800

    make it better

--- index.py
+++ index.py
@@ -4,7 +4,8 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-from flask import Flask,render_template, request
+from flask import Flask,render_template, request, make_response, jsonify
+    
 from datetime import datetime
 
 import requests
@@ -222,7 +223,18 @@ def movie_rate():
         db = firestore.client()
         doc_ref = db.collection("電影含分級").document(movie_id)
         doc_ref.set(doc)
-    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate    
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate 
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
